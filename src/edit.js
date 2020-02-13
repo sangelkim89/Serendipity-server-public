9145cddf5b4d653bf4be011e955db839e3c41a12
nohsikwan <tlrhks2869@naver.com>
2020-02-13 23:47:24 +0900

    subscription

--- src/edit.js
+++ src/edit.js
@@ -24,64 +24,56 @@ const upload = multer({
 });
 ////formData로 받을때 file로 받고  img가 아닌 Data는 body로 넘어온다.//////
 ////////////////////////upload.single는 한가지 req.file|||||||fields 는 여러개req.files
-export const uploadMiddleware = upload.fields([{ name: "profileImg" }, { name: "cardImg" }]);
+export const editUserMiddleware = upload.fields([{ name: "profileImg" }, { name: "cardImg" }]);
 
-export const uploadController = async (req, res) => {
+export const editUserController = async (req, res) => {
   const { cardImg, profileImg } = req.files;
 
   const cardImgLocation = cardImg[0].location;
   const profileImgLocation = profileImg[0].location;
 
   const {
-    name,
-    phone,
-    password,
-    email,
     gender,
+    email,
+    password,
+    phone,
+    name,
     birth,
-    bio,
     companyName,
     companyRole,
     geoLocation,
-    tags
+    tags,
+    bio
   } = req.body;
-  //name(닉네임)중복확인
+
   try {
-    ////singUp요청////
     const parseTags = JSON.parse(tags);
-    await prisma.createUser({
-      name,
-      phone,
-      password,
-      email,
-      gender,
-      birth,
-      bio,
-      companyName,
-      companyRole,
-      geoLocation,
-      cardImgLocation,
-      profileImgLocation,
-      tags: { set: parseTags }
+    await prisma.updateUser({
+      data: {
+        gender,
+        email,
+        password,
+        phone,
+        name,
+        birth,
+        companyName,
+        companyRole,
+        geoLocation,
+        tags: { set: parseTags },
+        profileImgLocation,
+        cardImgLocation,
+        bio
+      },
+      where: {
+        email
+      }
     });
-    ///////tag &&  user 연결 및 생성//////
-    // const parseTags = JSON.parse(tags);
-    // for (let i = 0; i < parseTags.length; i++) {
-    //   await prisma.createTag({
-    //     user: {
-    //       connect: {
-    //         email: email
-    //       }
-    //     },
-    //     tag: parseTags[i]
-    //   });
-    // }
 
     res.status(200).json({
       cardImgLocation,
       profileImgLocation
     });
   } catch (error) {
-    throw new Error("Can`t Create Account");
+    throw new Error("Can`t Edit User");
   }
 };
