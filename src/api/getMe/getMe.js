cef08198513c4f61b92789fc1a9eb82e236aa2f7
sangelkim89 <sangelkim89@gmail.com>
2020-02-18 15:14:31 +0900

    gittest2

--- src/api/getMe/getMe.js
+++ src/api/getMe/getMe.js
@@ -4,7 +4,6 @@ export default {
   Mutation: {
     getMe: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
-      console.log("testing git");
       const { user } = request;
       const userProfile = await prisma.user({ id: user.id });
       return userProfile;
