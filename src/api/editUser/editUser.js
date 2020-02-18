938f31727c73777ff5e8eb7663c6e215cae9f5e5
nohsikwan <tlrhks2869@naver.com>
2020-02-18 17:47:58 +0900

    editUserShema

--- src/api/editUser/editUser.js
+++ src/api/editUser/editUser.js
@@ -1,14 +1,20 @@
 import { prisma } from "../../../generated/prisma-client";
-
+import crypto from "crypto";
 export default {
   Mutation: {
     editUser: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { birth, companyName, companyRole, bio, password } = args;
+
+      // 해시 생성
+      const shasum = crypto.createHash("sha1");
+      shasum.update(password);
+      const output = shasum.digest("hex");
+
       try {
         await prisma.updateUser({
           data: {
-            password,
+            password: output,
             birth,
             companyName,
             companyRole,
