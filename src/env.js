9f63e2d96ecf58c07938ef977426030dc02d270d
sangelkim89 <sangelkim89@gmail.com>
2020-02-19 18:50:25 +0900

    heroku settings

--- src/env.js
+++ src/env.js
@@ -1,3 +1,4 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+// import path from 'path';
+dotenv.config();
+// used to be inside config(): { path: path.resolve(__dirname, '.env') }
