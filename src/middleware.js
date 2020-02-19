0d87c959fb8e2eb55f5bc7198f924d694bddd9ef
sangelkim89 <sangelkim89@gmail.com>
2020-02-19 23:06:04 +0900

    test for heroku

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 export const isAuthenticated = request => {
   //   console.log("request.headers in isAuth : ", request.headers);
   if (!request.user) {
+    console.log("test");
     throw Error("You need to log....");
   }
   return;
