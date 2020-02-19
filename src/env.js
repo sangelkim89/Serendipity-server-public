c72b19df03653c0740819052304a15387e053747
nohsikwan <tlrhks2869@naver.com>
2020-02-19 18:48:33 +0900

    prisma.yml

--- src/env.js
+++ src/env.js
@@ -1,3 +1,4 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+// import path from "path";
+dotenv.config();
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
