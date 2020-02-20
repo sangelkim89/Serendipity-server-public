3ae0f29f799bab7d6acbff7d0d75cb3adeb334c1
nohsikwan <tlrhks2869@naver.com>
2020-02-20 15:09:00 +0900

    likeRomm

--- src/api/likePlusRoom/likePlusRoom.js
+++ src/api/likePlusRoom/likePlusRoom.js
@@ -1,24 +1,31 @@
 import { prisma } from "../../../generated/prisma-client";
-
 export default {
   Mutation: {
     likeUser: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
-
+      //please
       const { user } = request;
       const { selectedId } = args;
       try {
-        await prisma.updateUser({
-          where: { id: user.id },
-          data: {
-            myLikes: {
-              connect: {
-                id: selectedId
-              }
-            }
+        const exists = await prisma.$exists.user({
+          myLikes_some: {
+            id: selectedId
           }
         });
-
+        if (!exists) {
+          await prisma.updateUser({
+            where: { id: user.id },
+            data: {
+              myLikes: {
+                connect: {
+                  id: selectedId
+                }
+              }
+            }
+          });
+        } else if (exists) {
+          return "you already like each other!";
+        }
         const youLikeMe = await prisma.$exists.user({
           AND: [
             {
@@ -47,13 +54,11 @@ export default {
         if (youLikeMe && mylikeBy) {
           // 	 const { user } = request;
           //   const { selectedId } = args;
-
           const room = await prisma.createRoom({
             participants: {
               connect: [{ id: user.id }, { id: selectedId }]
             }
           });
-
           return `${room.id}`;
         } else {
           return "The request has been successfully processed.";
