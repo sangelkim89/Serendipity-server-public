0e65429a827a473b7ecaa742713e65865512523f
nohsikwan <tlrhks2869@naver.com>
2020-02-21 17:07:42 +0900

    query

--- src/api/getMe/getMe.js
+++ src/api/getMe/getMe.js
@@ -1,8 +1,8 @@
 import { prisma } from "../../../generated/prisma-client";
 
 export default {
-  Mutation: {
-    getMe: async (_, args, { request, isAuthenticated }) => {
+  Query: {
+    getMe: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
       const userProfile = await prisma.user({ id: user.id });
